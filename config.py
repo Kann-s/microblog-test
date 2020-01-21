ae09eac5e6f95803c76be27dd2dafa207a22e69a
kann-s <sriramkannan72@gmail.com>
2020-01-21 12:38:07 +0000

    Database Config, Models and Migrations added
    Pip install flask_sqlalchemy and flask_migration in the venv.
    config.py amended to include config for sqlalchemy.
    app/__init__.py amended to instantiate db and migrate objects and
    register them with the app instance.
    app/models.py: Created models for User and Post entities.
    Ran 'flask db init' and 'flask db migrate' to create migration
    repository and generate upgrade() and downgrade() migrations.
    Ran 'flask db upgrade' to execute the upgrade scripts.
    Have not tried the 'flask db downgrade' command. but will try at some
    point in future.
    Tested the db and User, Post models interactively in the Python
    interpreter.
    microblog.py amended to import db, User and Post models. Also defined
    make_shell_context() so flask shell can import them directly.
    Deleted all the test data created in the database so we can continue
    with the tutorial.

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 import os
+import platform
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+platform_name = platform.system()
+if platform_name in ["Linux", "Darwin"]:
+    db_protocol = 'sqlite:////'
+elif platform_name == "Windows":
+    db_protocol = 'sqlite:///'
 
 class Config(object):
 
@@ -6,3 +15,7 @@ class Config(object):
     # against a nasty attack called Cross-Site Request Forgery
     # or CSRF (pronounced "seasurf")
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        db_protocol + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
